7616f522a489357bdcbf84a9f7e82b424ea20b3e
wrrr <cto@12flat.com>
2025-10-07 21:52:44 -0400

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,1 +1,8 @@
+import React from "react";
+export function Card({ children, ...props }) {
+  return <div {...props}>{children}</div>;
+}
+export function CardContent({ children, ...props }) {
+  return <div {...props}>{children}</div>;
+}
 
