26e4a487f38093f71cb31380df17cac96aa60f9b
wrrr <cto@12flat.com>
2025-10-07 21:54:10 -0400

    Update tabs.js

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,1 +1,12 @@
+import React, { useState } from "react";
+export function Tabs({ children, defaultValue, ...props }) {
+  const [active, setActive] = useState(defaultValue);
+  return <div {...props}>{children}</div>;
+}
+export function TabsList({ children, ...props }) {
+  return <div {...props}>{children}</div>;
+}
+export function TabsTrigger({ children, ...props }) {
+  return <button {...props}>{children}</button>;
+}
 
